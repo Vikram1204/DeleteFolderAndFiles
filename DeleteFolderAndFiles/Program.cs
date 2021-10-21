6ee1f7df1efde23e93e96b9299b79011f4a78c4e
Vikram <Vikram@LAPTOP-KREESHN>
2021-10-22 01:54:05 +0530

    Add project files.

---
+++ DeleteFolderAndFiles/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteFolderAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"G:\";
+            string[] patharr = { };
+
+            //if (Directory.Exists(path))
+            //{
+            //    Directory.Delete(path, true);
+            //    Console.WriteLine("Delete Success");
+            //}
+
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            var allFils = directory.EnumerateFiles();
+            var dir = directory.EnumerateDirectories();
+
+            //foreach (FileInfo file in allFils)
+            //{
+            //    Console.WriteLine(file.Name);
+                
+            //    //file.Delete();
+            //    //Console.WriteLine("Delete File Success");
+            //}
+
+            foreach (DirectoryInfo d in dir)
+            {
+               
+                //Console.WriteLine(d.FullName);
+                //d.Delete();
+                //Console.WriteLine("\nDelete Success");
+            }
+        }
+    }
+}
